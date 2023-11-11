fe6cd234f04573453b154088e0d2282aa01208f3
petarpetrov <ipetrovbg@gmail.com>
2023-11-11 11:04:25 +0200

    fix node lambda

--- src/rust_talk_node.ts
+++ src/rust_talk_node.ts
@@ -2,19 +2,26 @@ enum StatusCode {
   OK = 200,
 }
 
-type Response = {
-  statusCode: StatusCode;
+type ResponseModel = {
   message: string;
 };
 
+class Response {
+  statusCode: StatusCode;
+  body: string;
+
+  constructor(payload: ResponseModel, statusCode: StatusCode) {
+    this.statusCode = statusCode;
+    this.body = JSON.stringify(payload);
+  }
+}
+
 const functionHandler = async (): Promise<Response> => {
   return new Promise((resolve) => {
-    let message: Response = {
-      message: "Hello Rust Talk from Node!",
-      statusCode: StatusCode.OK,
-    };
+    let message: ResponseModel = { message: "Hello Rust Talk from Node!" };
+    let response = new Response(message, StatusCode.OK);
 
-    resolve(message);
+    resolve(response);
   });
 };
 
