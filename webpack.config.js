aa59c5973c2c32be4068e9509bf1057c3cd2a9f6
petarpetrov <ipetrovbg@gmail.com>
2023-11-11 15:35:09 +0200

    try fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   context: __dirname,
@@ -9,18 +10,12 @@ module.exports = {
     extensions: [".ts"],
   },
   target: "node",
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.(tsx?)$/,
         loader: "ts-loader",
-        exclude: [
-          [
-            path.resolve(__dirname, "node_modules"),
-            path.resolve(__dirname, ".serverless"),
-            path.resolve(__dirname, ".webpack"),
-          ],
-        ],
       },
     ],
   },
